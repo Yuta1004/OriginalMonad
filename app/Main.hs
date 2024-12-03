d9ee1bc226c7ce7abcc922c1a0815b77a0653e4f
NakagamiYuta <guguru0014@gmail.com>
2024-12-03 15:00:06 +0900

    [add] MyMaybe を作ってみた

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,40 @@
 module Main where
 
+-- Functor
+--  - fmap :: (a -> b) -> f a -> f b
+--      =
+
+-- Applicative
+--  - pure :: a -> f a
+--      =
+--  - (<*>) :: f (a -> b) -> f a -> f b
+--      =
+
+-- Monad
+--  - (>>=) :: m a -> (a -> m b) -> m b
+--      =
+
+data MyMaybe a = Some a | None
+    deriving (Show, Eq)
+
+instance Functor MyMaybe where
+    fmap f (Some a) = Some (f a)
+    fmap _ None = None
+
+instance Applicative MyMaybe where
+    pure = Some
+    (Some f) <*> (Some a) = Some (f a)
+    _ <*> _ = None
+
+instance Monad MyMaybe where
+    (Some a) >>= f = f a
+    None >>= _ = None
+
+mymain :: MyMaybe Int
+mymain = do
+    a <- Some 10
+    b <- Some 20
+    return $ a + b
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print mymain
