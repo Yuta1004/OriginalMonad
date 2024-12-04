cf87b2762024e0bf8025c9142ae957ca0919d37c
NakagamiYuta <guguru0014@gmail.com>
2024-12-04 16:53:39 +0900

    [update] MyMaybe をモジュール化

--- app/Main.hs
+++ app/Main.hs
@@ -1,40 +1,6 @@
 module Main where
 
--- Functor
---  - fmap :: (a -> b) -> f a -> f b
---      =
-
--- Applicative
---  - pure :: a -> f a
---      =
---  - (<*>) :: f (a -> b) -> f a -> f b
---      =
-
--- Monad
---  - (>>=) :: m a -> (a -> m b) -> m b
---      =
-
-data MyMaybe a = Some a | None
-    deriving (Show, Eq)
-
-instance Functor MyMaybe where
-    fmap f (Some a) = Some (f a)
-    fmap _ None = None
-
-instance Applicative MyMaybe where
-    pure = Some
-    (Some f) <*> (Some a) = Some (f a)
-    _ <*> _ = None
-
-instance Monad MyMaybe where
-    (Some a) >>= f = f a
-    None >>= _ = None
-
-mymain :: MyMaybe Int
-mymain = do
-    a <- Some 10
-    b <- Some 20
-    return $ a + b
+import MyMaybe
 
 main :: IO ()
-main = print mymain
+main = print runMyMaybe
---
+++ app/MyMaybe.hs
@@ -0,0 +1,33 @@
+module MyMaybe where
+
+-- Functor
+--  - fmap :: (a -> b) -> f a -> f b
+
+-- Applicative
+--  - pure :: a -> f a
+--  - (<*>) :: f (a -> b) -> f a -> f b
+
+-- Monad
+--  - (>>=) :: m a -> (a -> m b) -> m b
+
+data MyMaybe a = Some a | None
+    deriving (Show, Eq)
+
+instance Functor MyMaybe where
+    fmap f (Some a) = Some (f a)
+    fmap _ None = None
+
+instance Applicative MyMaybe where
+    pure = Some
+    (Some f) <*> (Some a) = Some (f a)
+    _ <*> _ = None
+
+instance Monad MyMaybe where
+    (Some a) >>= f = f a
+    None >>= _ = None
+
+runMyMaybe :: MyMaybe Int
+runMyMaybe = do
+    a <- Some 10
+    b <- Some 20
+    return $ a + b
